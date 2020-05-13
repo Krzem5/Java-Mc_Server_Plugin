a3d90d3a6a54976ad5519f7f677905e69d173df1
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-05-13 12:35:13 +0200

    Push Update 05/13/2020, 11:44:27

--- index.bat
+++ index.bat
@@ -2,5 +2,5 @@ echo off
 cd com&&echo NUL>_.class&&del /s /f /q *.class&&cd ../
 cls
 javac -cp ./build/;./; -d ./build/ com/krzem/mc_server_plugin/Main.java&&cd build&&jar cvf plugin.jar com/krzem/mc_server_plugin/Main.class * ../plugin.yml
-copy plugin.jar "C:\Users\Krzem\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\mcserver\plugins\"
+copy plugin.jar "C:\Users\aleks\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\mcserver\plugins\"
 cd D:\K\Coding\projects\Java-Mc_Server_Plugin\
